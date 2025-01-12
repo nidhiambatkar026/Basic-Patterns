07f090141668ea7962223bc0c913c73890012337
nidhiambatkar026 <nidhiambatkar@gmail.com>
2025-01-12 22:37:11 +0530

    variables added

--- solidrectangle.java
+++ solidrectangle.java
@@ -1,9 +1,10 @@
 //print solid rectangle
-import java.util.*;
 public class solidrectangle {
     public static void main(String args[]){
-        for(int i=1;i<=4;i++){
-            for(int j=1;j<=5;j++){
+        int n=4;
+        int m=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
                 System.out.print("*");
             }
             System.out.println(" ");
